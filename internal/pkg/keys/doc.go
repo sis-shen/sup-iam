60299a1beb5c32717742af1a0529a3ef486f6641
sis-shen <adongfa@gmail.com>
2026-05-19 23:02:20 +0800

    代码初始化：key加密代码封装成包

---
+++ internal/pkg/keys/doc.go
@@ -0,0 +1,2 @@
+// keys包提供可扩展的key生成接口
+package keys
---
+++ internal/pkg/keys/keys.go
@@ -0,0 +1,108 @@
+package keys
+
+import (
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
+type KeysInterface interface {
+	GenerateSecretKey() string
+	GenerateAccessKey() string
+	SignWithKey(secretKey string, payload string) (string, error)
+	VerifySecretKey(secretKey string, payload string, hashedStr string) (bool, error)
+}
+
+type Keys struct {
+	AccessKeyLength int `json:"access_key_length"` // AccessKey 长度（字节）,默认
+	SecretKeyLength int `json:"secret_key_length"`
+}
+
+func NewKeys(accessKeyLength int, secretKeyLength int) *Keys {
+	if accessKeyLength == 0 {
+		accessKeyLength = 32
+	}
+
+	if secretKeyLength == 0 {
+		secretKeyLength = 128
+	}
+
+	return &Keys{
+		AccessKeyLength: accessKeyLength,
+		SecretKeyLength: secretKeyLength,
+	}
+
+}
+
+var counter uint64
+
+func (k *Keys) GenerateSecretKey() string {
+	// 1. 获取纳秒时间戳并转换为16位十六进制
+	timestamp := uint64(time.Now().UnixNano())
+	timestampHex := fmt.Sprintf("%016x", timestamp)
+
+	// 2. 生成12位随机数（6字节）
+	randomBytes := make([]byte, k.SecretKeyLength-len(timestampHex)-4)
+	_, _ = rand.Read(randomBytes)
+	randomHex := hex.EncodeToString(randomBytes)
+
+	// 3. 获取4位计数器值（0-65535）
+	count := atomic.AddUint64(&counter, 1) % 65536
+	counterHex := fmt.Sprintf("%04x", count)
+
+	// 4. 组合成32位ID
+	return timestampHex + randomHex + counterHex
+}
+
+func (k *Keys) GenerateAccessKey() string {
+	// 1. 获取纳秒时间戳并转换为16位十六进制
+	timestamp := uint64(time.Now().UnixNano())
+	timestampHex := fmt.Sprintf("%016x", timestamp)
+
+	// 2. 生成12位随机数（6字节）
+	randomBytes := make([]byte, k.AccessKeyLength-len(timestampHex)-4)
+	_, _ = rand.Read(randomBytes)
+	randomHex := hex.EncodeToString(randomBytes)
+
+	// 3. 获取4位计数器值（0-65535）
+	count := atomic.AddUint64(&counter, 1) % 65536
+	counterHex := fmt.Sprintf("%04x", count)
+
+	// 4. 组合成32位ID
+	return timestampHex + randomHex + counterHex
+}
+
+func (k *Keys) SignWithKey(secretKey string, payload string) (string, error) {
+	if secretKey == "" {
+		return "", errors.New("secret key is empty")
+	}
+	if payload == "" {
+		return "", errors.New("payload is empty")
+	}
+
+	// 创建 HMAC-SHA256
+	h := hmac.New(sha256.New, []byte(secretKey))
+	h.Write([]byte(payload))
+
+	// 返回签名（hex 编码）
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func (k *Keys) VerifySecretKey(secretKey string, payload string, hashedStr string) (bool, error) {
+	if hashedStr == "" {
+		return false, errors.New("hashedStr key is empty")
+	}
+	check, err := k.SignWithKey(secretKey, payload)
+	if err != nil {
+		return false, err
+	}
+	if check != hashedStr {
+		return false, nil
+	}
+	return true, nil
+}
