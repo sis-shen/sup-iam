d44513200d5d787492ac2b6fd85e2702a12f0415
sis-shen <adongfa@gmail.com>
2026-05-28 15:23:42 +0800

    代码初始化：rpc的protobuf定义文件和自动生成代码初始化

---
+++ internal/pkg/proto/rpc/v1/gen.bat
@@ -0,0 +1,7 @@
+REM gen.bat
+protoc --proto_path=. ^
+        --go_out=. ^
+        --go_opt=paths=source_relative ^
+        --go-grpc_out=. ^
+        --go-grpc_opt=paths=source_relative ^
+        ./*.proto
