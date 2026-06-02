e696f7c455c00836df554a3bc140bd48baa48e85
sis-shen <adongfa@gmail.com>
2026-06-02 14:16:40 +0800

    代码初始化：server包初始化中断信号处理

---
+++ internal/pkg/server/signal.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var shutdownSignals = []os.Signal{os.Interrupt, syscall.SIGTERM}
+
+var onlyOneSignalHandler = make(chan struct{})
+
+var shutdownHandler chan os.Signal
+
+func SetupSignalHandler() <-chan struct{} {
+	close(onlyOneSignalHandler) // panic when call twice
+
+	shutdownHandler = make(chan os.Signal, 2)
+	stop := make(chan struct{})
+
+	signal.Notify(shutdownHandler, shutdownSignals...)
+
+	go func() {
+		<-shutdownHandler
+		close(stop)
+		<-shutdownHandler
+		os.Exit(0)
+	}()
+
+	return stop
+}
