490fb7c2c1dd4dfd06573d7077128f3560125a70
sis-shen <adongfa@gmail.com>
2026-06-04 14:23:42 +0800

    代码更新： 删除旧的pbv1代码

--- internal/iam-api-server/v1/rpc/adpter.go
+++ internal/iam-api-server/v1/rpc/adpter.go
@@ -3,12 +3,8 @@ package rpc
 import (
 	"context"
 	"fmt"
-	"github.com/sis-shen/sup-iam/internal/iam-api-server/v1/repository"
 	"github.com/sis-shen/sup-iam/internal/iam-api-server/v1/service"
-	pbv1 "github.com/sis-shen/sup-iam/internal/pkg/proto/rpc/v1"
 	"github.com/sis-shen/sup-iam/internal/pkg/proto/rpc/v2"
-	"strconv"
-	"time"
 )
 
 type AuthQueryHandler struct {
@@ -60,55 +56,4 @@ func (a *AuthQueryHandler) GetAllPolicies(ctx context.Context, req *pbv2.GetAllP
 		result = append(result, &pbv2.PolicyGroup{PolicyGroup: policies})
 	}
 	return &pbv2.GetAllPoliciesResponse{PolicyGroups: result}, nil
-}
-
-func (a *AuthQueryHandler) GetSecretByAK(ctx context.Context, in *pbv1.GetSecretByAKRequest) (*pbv1.GetSecretByAKResponse, error) {
-	secret, err := a.secretCase.GetSecretByAK(ctx, in.GetAccessKey())
-	if err != nil {
-		return nil, err
-	}
-
-	expiresAt := secret.UpdatedAt.Add(time.Duration(secret.Expires) * time.Second)
-	return &pbv1.GetSecretByAKResponse{
-		Secret: &pbv1.Secret{
-			SecretId:  strconv.FormatUint(secret.ID, 10),
-			SecretKey: secret.SecretKey,
-			AccessKey: secret.AccessKey,
-			ExpiresAt: expiresAt.Unix(),
-		},
-	}, nil
-}
-
-func (a *AuthQueryHandler) GetPolicyBySecretID(ctx context.Context, in *pbv1.GetPolicyBySecretIDRequest) (*pbv1.GetPolicyBySecretIDResponse, error) {
-	query := repository.PageQuery{
-		Limit:   100,
-		Cursor:  "",
-		OrderBy: "",
-		Order:   "",
-	}
-
-	id := in.SecretId
-
-	res, err := a.secretCase.GetSecretBindingPolicy(ctx, id, query)
-	if err != nil {
-		return nil, err
-	}
-
-	policyList := make([]*pbv1.Policy, 0)
-
-	for _, policy := range res.Items {
-		if policy.PolicyShadow == nil {
-			continue
-		}
-		p := &pbv1.Policy{
-			PolicyId:  strconv.FormatUint(policy.ID, 10),
-			Username:  policy.Username,
-			PolicyDsl: *policy.PolicyShadow,
-		}
-		policyList = append(policyList, p)
-	}
-
-	return &pbv1.GetPolicyBySecretIDResponse{
-		PolicyList: policyList,
-	}, nil
 }
